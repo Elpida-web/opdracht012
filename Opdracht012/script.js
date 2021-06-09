6721879bd71a4d84255cc52ef34fbf835c290efd
Elpida-web <84407349+Elpida-web@users.noreply.github.com>
2021-06-09 10:56:21 +0200

    Add files via upload

---
+++ Opdracht012/script.js
@@ -0,0 +1,29 @@
+
+
+const age = 99;
+const isFemale = true;
+
+if (age > 18 && isFemale) {
+
+    document.write("You can enter Zuipschuit");
+
+} else if (age == 18 && isFemale) {
+
+    document.write("You can enter Zuipschuit");
+
+} else {
+
+    document.write("You cannot enter");
+}
+
+const driverStatus = bob;
+
+if (driverStatus == bob) { 
+
+    document.write("You can drive");
+
+} else {
+
+document.write("Stay!!");
+
+}
